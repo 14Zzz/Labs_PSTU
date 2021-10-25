05dd29614e5f984ce8e4e8c0a643c6f9d83a77db
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-10-25 15:13:47 +0500

    Update Задача 1 (Full) - Комбинации цифр у числа.py

--- Задача 1 (Full) - Комбинации цифр у числа.py
+++ Задача 1 (Full) - Комбинации цифр у числа.py
@@ -1,115 +1,49 @@
 # Ввод числа, в котором цифры не повторяются. Поиск возможных комбинаций. Поиск макс комбинации.
-# Не знаешь что пробовать - попробуй ВСЁ
-# Читаемость кода в сделку не входила
-import time  # чисто покекать над числом из 10ти цифр)) Хотя я не дождался((
+import time  # чисто что бы было
 
 
-def F(M):  # Проверка на совподения цифр в комбинации
+# Функция по принципу дерева перебирает все возможные комбинации
+# (берется цифра => строятся все её вариации с другими цифрами из списка, но не включая уже имеющиеся цифры =>
+# то же самое повторяется для новых построенных значений,
+# до тех пор, пока длинна построенной строки не совпадет с длинной списка цифр =>
+# PROFFIT
+
+def F(n):  # n - первая цифра в списке
+    if len(n) == len(l):
+        print(n)
+    for i in range(len(l)):
+        x = n
+        if l[i] not in x:
+            x += l[i]
+            F(x)
+
+
+def Sovpadenie(M):  # Проверка на совподения цифр в комбинации
     for j in range(11):
         if M.count(str(j)) > 1:
             return True
 
 
-while True:    # Кручусь в цикле просто потому что могу :^)
+while True:  # Кручусь в цикле просто потому что могу :^)
     try:
-        x = int(input("Введите целое число, цифры которого не повторяются: "))
+        x = int(input("Введите целое число, цифры которого не повторяются: "))  # Ввод числа
         start_time = time.time()
-        if x < 0:
+        if x < 0:  # Проверка на отрицательность
             x *= -1
-        x = str(x)
+        x = str(x)  # Работаем со строкой
         l = []
-        for i in range(0, len(x)):
-            l.append(x[i])
-        if not F(l):
-            biggest = 1
-            for a1 in range(len(x)):
-                if len(x) == 1:
-                    print(l[a1])
-                    if int(l[a1]) > biggest:
-                        biggest = int(l[a1])
-                for a2 in range(len(x)):
-                    if len(x) == 2:
-                        if a1 != a2:
-                            print(l[a1], l[a2])
-                            if int(l[a1] + l[a2]) > biggest:
-                                biggest = int(l[a1] + l[a2])
-                    for a3 in range(len(x)):
-                        if len(x) == 3:
-                            if a1 != a2 != a3 != a1:
-                                print(l[a1], l[a2], l[a3])
-                                if int(l[a1] + l[a2] + l[a3]) > biggest:
-                                    biggest = int(l[a1] + l[a2] + l[a3])
-                        for a4 in range(len(x)):
-                            if len(x) == 4:
-                                M = [l[a1], l[a2], l[a3], l[a4]]
-                                if not F(M):
-                                    print(l[a1], l[a2], l[a3], l[a4])
-                                    if int(l[a1] + l[a2] + l[a3] + l[a4]) > biggest:
-                                        biggest = int(l[a1] + l[a2] + l[a3] + l[a4])
-                            for a5 in range(len(x)):
-                                if len(x) == 5:
-                                    M = [l[a1], l[a2], l[a3], l[a4], l[a5]]
-                                    if not F(M):
-                                        print(l[a1], l[a2], l[a3], l[a4], l[a5])
-                                        if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5]) > biggest:
-                                            biggest = int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5])
-                                for a6 in range(len(x)):
-                                    if len(x) == 6:
-                                        M = [l[a1], l[a2], l[a3], l[a4], l[a5], l[a6]]
-                                        if not F(M):
-                                            print(l[a1], l[a2], l[a3], l[a4], l[a5], l[a6])
-                                            if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6]) > biggest:
-                                                biggest = int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6])
-                                    for a7 in range(len(x)):
-                                        if len(x) == 7:
-                                            M = [l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7]]
-                                            if not F(M):
-                                                print(l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7])
-                                                if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[
-                                                    a7]) > biggest:
-                                                    biggest = int(
-                                                        l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[a7])
-                                        for a8 in range(len(x)):
-                                            if len(x) == 8:
-                                                M = [l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7], l[a8]]
-                                                if not F(M):
-                                                    print(l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7], l[a8])
-                                                    if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[a7] +
-                                                           l[a8]) > biggest:
-                                                        biggest = int(
-                                                            l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[a7] +
-                                                            l[a8])
-                                            for a9 in range(len(x)):
-                                                if len(x) == 9:
-                                                    M = [l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7], l[a8],
-                                                         l[a9]]
-                                                    if not F(M):
-                                                        print(l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7],
-                                                              l[a8], l[a9])
-                                                        if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[
-                                                            a7] + l[
-                                                                   a8] + l[a9]) > biggest:
-                                                            biggest = int(
-                                                                l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] + l[
-                                                                    a7] + l[a8] +
-                                                                l[a9])
-                                                for a10 in range(len(x)):
-                                                    if len(x) == 10:
-                                                        M = [l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7], l[a8],
-                                                             l[a9], l[a10]]
-                                                        if not F(M):
-                                                            print(l[a1], l[a2], l[a3], l[a4], l[a5], l[a6], l[a7],
-                                                                  l[a8], l[a9],
-                                                                  l[a10])
-                                                            if int(l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] +
-                                                                   l[a7] + l[
-                                                                       a8] + l[a9] + l[a10]) > biggest:
-                                                                biggest = int(
-                                                                    l[a1] + l[a2] + l[a3] + l[a4] + l[a5] + l[a6] +
-                                                                    l[a7] + l[a8] +
-                                                                    l[a9] + l[a10])
-            print("Наибольшая комбинация =", biggest)
-            print("--- %s seconds ---" % (time.time() - start_time))  # Но кека можно и не дождаться KekWait
+        biggest = ""
+        for k in range(len(x)):  # Заполнение списка цифрами для комбинации
+            l.append(x[k])
+        if not Sovpadenie(l):  # Проверка на совпадения цифр в числе
+            for m in range(len(l)):  # Поочерердно закидываем элементы в функцию и исключаем 0
+                if l[m] != '0':  # C нуля цифры не начинаются - Да ладно?)
+                    F(l[m])
+            l.sort()
+            for o in range(len(l)):  # Составление наименьшей строки (потом перевернём)
+                biggest += l[o]
+            print("Наибоьшая комбинация:", biggest[::-1])  # Переворачиваем
+            print("--- %s seconds ---" % (time.time() - start_time))
         else:
             print()
             print("///Цифры в числе не должны повторяться///")
