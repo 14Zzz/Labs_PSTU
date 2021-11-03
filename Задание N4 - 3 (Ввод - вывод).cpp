377e4323470d34a5396e56a0d05b5c45b6716d3e
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-03 15:07:17 +0500

    Add files via upload

---
+++ Задание N4 - 3 (Ввод - вывод).cpp
@@ -0,0 +1,12 @@
+#include <iostream>		// Добавление библиотеки iostream
+using namespace std;	// Объявления пространства имен std
+
+int main()	// Обзначаем main функцию
+{
+	const int x = 10;	// Константа
+	int b;	// Обьявление b
+	b = 20;	// Присваивание 
+	cout << x<< endl;	// Вывод и кончаем эту строку
+	cout << b<<endl;
+	cout << --b; //ДеКрЕмЕнТ (сказали бы -1 и ОК)
+}
