93a86e4ea7d0a1101bfd41c4bc8e529408396520
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2022-02-15 04:16:00 +0500

    Create  7.2. "Функции с переменным числом параметров".cpp

---
+++ 7.2. "Функции с переменным числом параметров"/7.2. "Функции с переменным числом параметров".cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<complex>
+using namespace std;
+
+int min (int n, int first, ...)
+{
+    int* ptr = &first;
+    int mi = *ptr;
+    for (n; n != 0; n--)    // Можно while (n--)
+    {
+        if (mi > *ptr)
+        {
+            mi = *ptr;
+        }
+        ptr += 2;   // int = 2 байта => +2 на сдвиг??
+    }
+    return mi;
+}
+
+
+int main()
+{
+    cout << min(5, 9, 4, 14, 7689, 4) << endl;
+    cout << min(10, -1, 14, 1, 18, -27, 26, 0, 8, 9, 50) << endl;
+    cout << min(12, 65, 78, 345, 87, 34, 23, 89, 34, 45, 234, 768, 12) << endl;
+}
