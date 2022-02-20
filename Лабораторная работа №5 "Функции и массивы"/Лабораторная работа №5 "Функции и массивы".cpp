120be41f209b0b06cd6de841ac7f1f67ba511a8f
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2022-02-21 00:58:09 +0500

    Update Лабораторная работа №5 "Функции и массивы".cpp

--- Лабораторная работа №5 "Функции и массивы"/Лабораторная работа №5 "Функции и массивы".cpp
+++ Лабораторная работа №5 "Функции и массивы"/Лабораторная работа №5 "Функции и массивы".cpp
@@ -1,1 +1,50 @@
+#include<iostream>
 
+using namespace std;
+
+// Функция
+int sedlovie (int strok, int stolbcov, int a[6][7])
+{
+    for (int j = 0; j < strok; j++)
+    {
+        for (int i = 0; i < stolbcov; i++)
+        {
+            cout << a[j][i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // Ввод числа элементов
+    int strok = 6, stolbcov = 7;
+    
+    // Создание массива
+
+    int a[strok][stolbcov];
+    for (int j = 0; j < strok; j++)
+    {
+        for (int i = 0; i < stolbcov; i++)
+        {
+            a[j][i]= rand() % 100;
+        }
+    }
+
+    // Вывод массива
+    cout << "Сформированная матрица" << endl;
+
+    for (int j = 0; j < strok; j++)
+    {
+        for (int i = 0; i < stolbcov; i++)
+        {
+            cout << a[j][i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+    sedlovie(strok, stolbcov, a);
+}
