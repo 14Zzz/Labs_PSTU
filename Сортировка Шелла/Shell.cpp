264eb4410951e79a0298fb4a55f5e76379ec1237
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2022-02-27 00:07:46 +0500

    Create Shell.cpp

---
+++ Сортировка Шелла/Shell.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+void ShelSort(int a[], int n)
+{
+    int i, j, d, tmp;
+    d = n / 2;
+    while (d > 0)
+    {
+        for (i = 0; i < n - d; i++)
+        {
+            j = i;
+            while (j >= 0 && a[j] > a[j+d])
+            {
+                tmp = a[j];
+                a[j] = a[j + d];
+                a[j + d] = tmp;
+                j--;
+            }
+        }
+        d = d / 2;
+    }
+    cout << "Отсортированный массив из " << n << " элементов = [";
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << a[i] << ", ";
+    }
+    cout << a[n - 1] << "]" << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int n, d, tmp = -1, o = 1;
+    cout << "Введите количество элементов массива: ";
+    cin >> n;
+    
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 100;
+    }
+    
+    cout << "Сформированный массив из " << n << " элементов = [";
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << a[i] << ", ";
+    }
+    cout << a[n - 1] << "]" << endl;
+    ShelSort(a, n);
+}
