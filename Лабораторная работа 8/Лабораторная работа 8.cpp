ca65fb9ac6a83bb7acc31170b1d74bd33fd2a5a1
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2022-03-13 21:29:16 +0500

    Update Лабораторная работа 8.cpp

--- Лабораторная работа 8/Лабораторная работа 8.cpp
+++ Лабораторная работа 8/Лабораторная работа 8.cpp
@@ -39,11 +39,14 @@ int main()
 {
     Skolnik[0] = {"Еремеев Дмитрий Сергеевич", "9В", "86483965735", "4, 4, 2, 3"};
     Skolnik[1] = {"Полушкин Игорь Александрович", "11А", "89765685382", "4, 4, 5, 5"};
-    Skolnik[2] = {"Красов Александр Игоревич", "10В", "81749628381", "5, 5, 5, 5"};
+    Skolnik[2] = {"Красов Александр Игоревич", "10В", "81749628381", "2, 5, 5, 5"};
     Skolnik[3] = {"Патласов Владислав Максимович", "9Г", "89468317184", "4, 4, 4, 4"};
     Skolnik[4] = {"Чащухин Максим Николаевич", "11А", "8904123456", "2, 3, 4, 4"};
     
     fstream file("file.txt", ios::trunc | ios::in | ios::out);
+    
+    fstream file1("file1.txt", ios::trunc | ios::in | ios::out);
+    
     if (file.is_open())
     {
         cout << "Файл открыт" << endl;
@@ -76,14 +79,29 @@ int main()
         Skolnik[5].otmetki = str;
         
         cout << endl << endl << "Новое cодержимое файла: " << endl << endl;
-
+        
+        
         file.clear();
-        AddTXT(file, Skolnik[5]);
+        AddTXT(file1, Skolnik[5]);
         
         for (int i = 0; i < 5; i++)
         {
-            AddTXT(file, Skolnik[i]);
+            str = Skolnik[i].otmetki;
+            for (int j = 0; j <= str.length(); j++)
+            {
+                if (str[j] == '2')
+                {
+                    break;
+                }
+                else if (j == str.length())
+                {
+                    AddTXT(file1, Skolnik[i]);
+                }
+            }
         }
+        
+        remove("file.txt");
+        rename("file1.txt", "file.txt");
         
         file.close();
         file.clear();
