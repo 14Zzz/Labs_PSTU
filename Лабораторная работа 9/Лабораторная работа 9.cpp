f077b38d4f61995432edde1c10df6365de016651
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2022-03-15 02:22:30 +0500

    Update Лабораторная работа 9.cpp

--- Лабораторная работа 9/Лабораторная работа 9.cpp
+++ Лабораторная работа 9/Лабораторная работа 9.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
 #include <string>
 #include<fstream>
-
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    int N, K;
-    string s1;
-    
-    fstream file1("file1.txt", ios::trunc | ios::in | ios::out);
-    fstream file2("file2.txt", ios::trunc | ios::in | ios::out);
-    
-    cout << "Введите 10 строк на русском" << endl;
-    for (int i = 1; i < 11; i++)
-    {
-        cout << "Строка " << i << " = ";
-        getline(cin, s1);
-        file1 << s1 << endl;
-    }
     setlocale(LC_ALL, "rus");
-    cout << "Введите номер строки, с которой начать копирование: ";
-    cin >> N;
-    cout << "Введите номер строки, на которой закончить копирование: ";
-    cin >> K;
-    
-    file1.clear();
-    for (int i = 1; i < 11; i++)
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int N, K, k;
+    string s1;
+    int counter = 0;
+    string SOGL = "бвгджзйклмнпрстфхцчшщБВГДЖЗЁКЛМНПРСТФХЦЧШЩ";
+    ofstream file1;
+    file1.open("file1.txt");
+
+    cout << "Введите количество строк: ";
+    cin >> k;
+    cout << "Введите " << k << " строк" << endl;
+    getline(cin, s1);
+    if (file1.is_open())
     {
-        getline(file1, s1);
-        file1 << s1 << endl;
+        for (int i = 1; i < k + 1; i++)
+        {
+            cout << "Строка " << i << " = ";
+            getline(cin, s1);
+            file1 << s1 << endl;
+        }
+        cout << "Введите номер строки с которой начать копирование: ";
+        cin >> N;
+        cout << "Введите номер строки на которой закончить копирование: ";
+        cin >> K;
+
+        ifstream file1;
+        file1.open("file1.txt");
+
+        ofstream file2;
+        file2.open("file2.txt");
+
+        for (int i = 1; i < k + 1; i++)
+        {
+            if (i >= N && i <= K)
+            {
+                getline(file1, s1);
+                file2 << s1 << endl;
+                for (int m = 0; m < SOGL.length(); m++)
+                {
+                    for (int j = 0; j < s1.length(); j++)
+                    {
+                        if (s1[j] == SOGL[m])
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                getline(file1, s1);
+            }
+        }
+        cout << "Количество согласных во 2-ом файле = " << counter;
+        file1.close();
+        file2.close();
     }
-    
-    file1.close();
-    file2.close();
+    else
+    {
+        cout << "Ошибка открытия файла!";
+    }
 }
